f504b29c8d00019e53e433f773947b1a0bdac0de
Mehak Dhull <mehakdhull189@gmail.com>
2026-06-15 12:18:45 -0400

    Add Expenses page and update App with latest changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import ProtectedRoute from './components/ProtectedRoute'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
-
+import Expenses from './pages/Expenses'
 
 
 import Activity from './pages/Activity'
